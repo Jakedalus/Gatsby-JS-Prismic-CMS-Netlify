d3890688e34e61edfe91a5f5cef174f37e5827b1
Jakedalus <jacquedalus@gmail.com>
2020-03-20 17:53:18 -0400

    Add basic Page component

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const Page = () => {
+export const query = graphql`
+query PageQuery($id: String){
+  prismic {
+    allPages(id: $id) {
+      edges {
+        node {
+          page_title
+          _meta {
+            uid
+            id
+          }
+        }
+      }
+    }
+  }
+}`;
+
+const Page = props => {
+  console.log('Page, props:', props);
   return (
     <div>
       <h1>
