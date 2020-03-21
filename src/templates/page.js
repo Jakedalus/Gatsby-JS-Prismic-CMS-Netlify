d04dc81251f5976e8bcf82c5c71a1727690c3a7e
Jakedalus <jacquedalus@gmail.com>
2020-03-21 17:12:14 -0400

    Update layout component and styles

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { RichText } from 'prismic-reactjs';
+import Layout from '../components/layout';
 
 export const query = graphql`
 query PageQuery($id: String){
@@ -25,10 +26,10 @@ const Page = props => {
   const pageTitle = props.data.prismic.allPages.edges[0].node.page_title;
   const content = props.data.prismic.allPages.edges[0].node.content;
   return (
-    <>
+    <Layout>
       <RichText render={pageTitle} />
       <RichText render={content} />
-    </>
+    </Layout>
   )
 }
 
