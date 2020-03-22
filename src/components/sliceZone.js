80adf47b00ae5d0f26cb0307e9ba27c3c379d0da
Jakedalus <jacquedalus@gmail.com>
2020-03-21 21:00:25 -0400

    Setup hero

---
+++ src/components/sliceZone.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Hero from './hero';
+
+const SliceZone = ({body}) => {
+  console.log(body);
+  return (
+    <div>
+      {body.map((bodyContent, i) => {
+        if (bodyContent.type === 'hero') {
+          return (
+            <Hero 
+              key={i}
+              title={bodyContent.primary.hero_title}
+              content={bodyContent.primary.hero_content}
+            />
+            )
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
+};
+
+export default SliceZone;
